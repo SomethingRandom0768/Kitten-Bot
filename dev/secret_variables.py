0ba55c470b37a96f2b72c8ac8c4327aacee21df1
SomethingRandom <shishkebab259@gmail.com>
2021-12-29 23:32:40 -0600

    General: Hiding development directory

--- dev/secret_variables.py
+++
@@ -1,4 +0,0 @@
-client_ID = 'IVJMP4juDRfx-w'
-client_secret = '2q_P6io3kdE1YMd1m6NMOXfBaj0HjA'
-discord_token = 'ODAyNjEyNjIxODQ5MDAyMDEy.YAxxQA.VN0TONoU53MQjYL1Purm9rmMNlw'
-user_agent = 'Kitten Bot by SomethingRandom0768'
